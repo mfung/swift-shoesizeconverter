b0cb6f31ecfd3efc6a2cb41426958bd061840ace
Meng Fung <meng.fung@gmail.com>
2014-09-23 14:39:22 -0700

    First commit, added shoe size converter for mens shoe size from US to EU

--- ShoeSizeConverter/ViewController.swift
+++ ShoeSizeConverter/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var mensShowSizeTextField: UITextField!
+  @IBOutlet weak var convertedShoeSizeLabel: UILabel!
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,13 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  @IBAction func convertButtonPress(sender: UIButton) {
+    let intFromTextField = mensShowSizeTextField.text.toInt()!
+    let conversionConstant = 30
+    
+    var convertedShoeSize = intFromTextField + conversionConstant
+    convertedShoeSizeLabel.hidden = false
+    convertedShoeSizeLabel.text = "\(convertedShoeSize) in EU Shoe Size"
+  }
 
 }
-
